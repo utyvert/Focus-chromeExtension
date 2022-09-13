0907d45839c4ba0057bab4f4e1e38d8e2f9f7dfb
utyvert <utkarshuppal@gmail.com>
2022-09-12 18:01:19 -0700

    added image and text to blocker

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-const contents = document.getElementById('contents');
+const contents = $('#contents');
+var contentsParent = contents.parent();
 contents.remove();
+// contentsParent.prepend(<img src="motiv.jpg" alt="quote" ></img>)
+contentsParent.prepend('<img src="https://i.pinimg.com/originals/d8/9f/4e/d89f4e15078c17b1b4a09ca32647063c.jpg"/>');
+// contentsParent.prepend('<p class = "beautText">Get back to work. You know what you need to do.</p>')
+contentsParent.prepend('<div class = "beautText"> Get back to work. You know what you need to do. </div>')
--- twitter.js
+++ twitter.js
@@ -1,2 +1,6 @@
-const contentsTwo = document.getElementById('react-root')
+const contentsTwo = $('#react-root')
+const contents2Parent = contentsTwo.parent;
 contentsTwo.remove();
+// contents2Parent.prepend('<img src="https://i.pinimg.com/originals/d8/9f/4e/d89f4e15078c17b1b4a09ca32647063c.jpg"/>');
+// contents2Parent.prepend('<div class = "beautText"> Get back to work. You know what you need to do. </div>')
+
